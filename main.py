4f60ff433a2ccddb71b1fcb5ab17ab096f01fa58
John Wregglesworth <wregglej@cyverse.org>
2025-08-19 18:23:16 -0700

    Update the error handling

--- main.py
+++ main.py
@@ -4,9 +4,30 @@ import portal_ldap
 import kinds
 import os
 
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, Request
+from fastapi.responses import JSONResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request: Request, exc: StarletteHTTPException):
+    print(exc)
+    return JSONResponse(
+        content={"detail": exc.detail}, status_code=exc.status_code
+    )
+
+
+@app.middleware("http")
+async def exception_handling_middleware(request: Request, call_next):
+    try:
+        return await call_next(request)
+    except Exception as e:
+        print(str(e))
+        return JSONResponse(content=str(e), status_code=500)
+
 
 ldap_url = os.environ.get("LDAP_URL")
 ldap_user = os.environ.get("LDAP_USER")
@@ -24,110 +45,62 @@ def hello():
 @app.post("/users", status_code=status.HTTP_201_CREATED)
 def create_user(user: kinds.CreateUserRequest):
     dse = f"{portal_ldap.days_since_epoch()}"
-    try:
-        portal_ldap.create_user(ldap_conn, ldap_base_dn, dse, user)
-    except LDAPError as err:
-        raise HTTPException(
-            status_code=400, detail=f"desc: '{err.desc}', info: '{err.info}'"
-        )
+    portal_ldap.create_user(ldap_conn, ldap_base_dn, dse, user)
     return {"user": user.username}
 
 
 @app.get("/users", status_code=200)
 def list_users():
-    try:
-        users = portal_ldap.list_users(ldap_conn, ldap_base_dn)
-    except LDAPError as err:
-        raise HTTPException(
-            status_code=400, detail=f"desc: '{err.desc}' info: '{err.info}'"
-        )
+    users = portal_ldap.list_users(ldap_conn, ldap_base_dn)
     return {"users": users}
 
 
 @app.get("/users/{username}", status_code=200)
 def get_user(username: str):
-    try:
-        user = portal_ldap.get_user(ldap_conn, ldap_base_dn, username)
-    except LDAPError as err:
-        raise HTTPException(status_code=400, detail=f"{err}")
+    user = portal_ldap.get_user(ldap_conn, ldap_base_dn, username)
     return {"user": user}
 
 
 @app.get("/users/{username}/groups", status_code=200)
 def get_user_groups(username: str):
-    try:
-        groups = portal_ldap.get_user_groups(ldap_conn, ldap_base_dn, username)
-    except LDAPError as err:
-        raise HTTPException(500, err)
+    groups = portal_ldap.get_user_groups(ldap_conn, ldap_base_dn, username)
     return {"user": username, "groups": groups}
 
 
 @app.get("/groups", status_code=200)
 def list_groups():
-    try:
-        groups = portal_ldap.get_groups(ldap_conn, ldap_base_dn)
-    except LDAPError as err:
-        raise HTTPException(
-            status_code=400, detail=f"desc: '{err.desc}', info: '{err.info}'"
-        )
+    groups = portal_ldap.get_groups(ldap_conn, ldap_base_dn)
     return {"groups": groups}
 
 
 @app.post("/groups/{group_name}/users/{username}", status_code=200)
 def add_user_to_group(group_name: str, username: str):
-    try:
-        portal_ldap.add_user_to_group(
-            ldap_conn, ldap_base_dn, username, group_name
-        )
-    except LDAPError as err:
-        raise HTTPException(
-            status_code=400, detail=f"desc: '{err.desc}', info: '{err.info}'"
-        )
+    portal_ldap.add_user_to_group(ldap_conn, ldap_base_dn, username, group_name)
     return {"group": group_name, "user": username}
 
 
 @app.delete("/groups/{group}/users/{username}", status_code=200)
 def remove_user_from_group(username: str, group: str):
-    try:
-        portal_ldap.remove_user_from_group(
-            ldap_conn, ldap_base_dn, username, group
-        )
-    except LDAPError as err:
-        raise HTTPException(
-            status_code=400, detail=f"desc: '{err.desc}', info: '{err.info}'"
-        )
+    portal_ldap.remove_user_from_group(ldap_conn, ldap_base_dn, username, group)
     return {"group": group, "username": username}
 
 
 @app.delete("/users/{username}")
 def delete_user(username: str):
-    try:
-        portal_ldap.delete_user(ldap_conn, ldap_base_dn, username)
-    except LDAPError as err:
-        raise HTTPException(
-            status_code=400, detail=f"desc: '{err.desc}', info: '{err.info}'"
-        )
+    portal_ldap.delete_user(ldap_conn, ldap_base_dn, username)
     return {"user": username}
 
 
 @app.put("/users/{username}/password", status_code=200)
 def change_password(username: str, password: kinds.SimplePassword):
-    try:
-        portal_ldap.change_password(
-            ldap_conn, ldap_base_dn, username, password.password
-        )
-    except LDAPError as err:
-        raise HTTPException(status_code=400, detail=f"{err}")
+    portal_ldap.change_password(
+        ldap_conn, ldap_base_dn, username, password.password
+    )
     return {"user": username}
 
 
 @app.post("/users/{user_id}/shadow-last-change")
 def shadow_last_change(user_id: str):
     dse = f"{portal_ldap.days_since_epoch()}"
-    try:
-        portal_ldap.shadow_last_change(ldap_conn, ldap_base_dn, dse, user_id)
-    except LDAPError as err:
-        raise HTTPException(
-            status_code=400, detail=f"desc: '{err.desc}', info: '{err.info}'"
-        )
+    portal_ldap.shadow_last_change(ldap_conn, ldap_base_dn, dse, user_id)
     return {"user": user_id}
